0d4f4f82434175cb97209341c43fa3c4e1392676
unknown <khanhvan0731@gmail.com>
2020-03-02 23:22:07 +0700

    update 2/3

--- src/main/java/demo/Repository/OrderRepository.java
+++ src/main/java/demo/Repository/OrderRepository.java
@@ -14,6 +14,7 @@ public interface OrderRepository extends JpaRepository<Orders, Long> {
     @Query(value = "select * from Orders", nativeQuery = true)
     Set<Orders> findAllOrders();
 
-    @Query(value = "select * from Orders o, Bike b where o.bike_id = b.id and o.number >= ?1 and b.purchase_Price >= ?2 and b.name = ?3", nativeQuery = true) // ?1 (param thứ 1 truyền vào) = number
-    Set<Orders> searchOrders(Long number, BigDecimal price, String name ); // number = 1 => select * from Orders where number > 1
+    @Query(value = "select * from Orders o, Bike b where o.bike_id = b.id and (?1 is null or o.number >= ?1)" +
+            " and (?2 is null or b.purchase_Price >= ?2) and (?3 is null or b.name = ?3)", nativeQuery = true)
+    Set<Orders> searchOrders(Long number, BigDecimal price, String name );
 }
